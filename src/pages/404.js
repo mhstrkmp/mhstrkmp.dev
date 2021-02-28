54353b7e68e444a62841ab79a9f51e3e2ccc0db3
mhstrkmp <hello@mhstrkmp.dev>
2021-02-28 23:49:57 +0100

    🤡 Add dummy pages

---
+++ src/pages/404.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+
+const FourOFourPage = () => {
+  return (
+    <>
+      <main className="prose">
+        <h2>Hello world!</h2>
+        <h3>A Subheading ...</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
+          architecto ullam nihil soluta ab possimus! Aliquid obcaecati ea, eum a
+          voluptas beatae consequatur voluptatum quis totam, labore delectus aut
+          architecto ut ipsam itaque eaque et minima, assumenda ducimus sequi
+          quidem perferendis temporibus soluta? Veniam id quam consectetur autem
+          molestias officiis deserunt possimus. Incidunt architecto ex
+          laboriosam fugiat beatae dolorum, natus voluptatum consequuntur
+          quibusdam! Repellendus maxime facilis aliquam architecto delectus
+          harum blanditiis, illum esse alias. Officiis laudantium consequatur
+          repellat totam sint, voluptatem ut natus, doloribus, repudiandae ipsum
+          numquam? Voluptatibus aperiam praesentium recusandae ipsam aliquam
+          repudiandae, impedit odio! Aliquam assumenda tempora qui.
+        </p>
+        <p>
+          Maiores, enim reprehenderit nemo sequi veniam asperiores eos illo
+          optio, libero vel iste sit nostrum sunt voluptatem! Sit qui quod quasi
+          amet ea sint officia. Eius harum doloremque deserunt tenetur placeat
+          quos mollitia accusamus unde. Dolore pariatur aut quisquam fugiat,
+          dicta rem fugit aliquid autem atque tempore expedita quam placeat
+          asperiores alias maxime. Culpa tempore alias deleniti sed aliquid
+          vitae tenetur. Similique optio aspernatur placeat, amet temporibus
+          nemo numquam corporis illum harum perspiciatis aperiam praesentium,
+          explicabo ad! Consequuntur, quisquam laboriosam? Qui repellendus minus
+          at deleniti reiciendis repudiandae placeat cupiditate, dolores
+          exercitationem ipsum repellat porro sed quaerat amet quae voluptas
+          vel.
+        </p>
+        <p>
+          Voluptatum soluta dolore placeat rem inventore ipsum in excepturi
+          quaerat, earum recusandae, impedit natus molestiae, quasi dolor saepe
+          sapiente autem iste. Corrupti in ipsam sint ratione placeat porro,
+          eveniet aliquam praesentium unde dolorem dolor, assumenda molestiae,
+          neque consequuntur at! Saepe dicta perferendis non doloremque
+          corporis! Ea exercitationem ipsa explicabo facere aspernatur repellat
+          saepe, obcaecati iusto odit ab debitis earum corporis veniam,
+          perspiciatis possimus, nesciunt minus sit illo a doloribus in
+          repudiandae placeat iure! Tenetur explicabo, reprehenderit commodi
+          obcaecati optio odio! Aspernatur earum, nesciunt dolorum pariatur quis
+          magni. Tenetur, omnis odio impedit asperiores, ipsum, ratione libero
+          eligendi doloribus quisquam delectus ea!
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default FourOFourPage;
