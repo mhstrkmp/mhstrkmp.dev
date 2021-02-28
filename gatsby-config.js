a5f245fab4c53ee0984e91009a2b9f3dd44f5090
mhstrkmp <hello@mhstrkmp.dev>
2021-02-28 22:46:11 +0100

    ➕ Add sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  plugins: ["gatsby-plugin-postcss"],
+  siteMetadata: {
+    siteUrl: "https://mhstrkmp.dev",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://mhstrkmp.dev",
+        sitemap: "https://mhstrkmp.dev/sitemap.xml",
+        env: {
+          development: {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+          },
+          production: {
+            policy: [{ userAgent: "*", disallow: "/" }],
+          },
+        },
+      },
+    },
+  ],
 };
