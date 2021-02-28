dead0e1b34708ff0962412dbd06abab1e2895314
mhstrkmp <hello@mhstrkmp.dev>
2021-02-28 22:58:35 +0100

    🚧 Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container w-full mx-auto">
+      <footer className="py-6  text-center">
+        <span>Made with ❤ by️ Matthias Heisterkamp</span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = ({ children }) => (
   <div className="container w-full max-w-screen-lg mx-auto px-8 ">
     <Header />
     {children}
+    <Footer />
   </div>
 );
 
