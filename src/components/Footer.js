255366ccc42e3daebc5f8a6192dbdc8dd99512c6
mhstrkmp <hello@mhstrkmp.dev>
2021-03-02 21:39:25 +0100

    ♻️ Refactor footer to show social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,42 @@
 import React from "react";
 
+const today = new Date();
+
 const Footer = () => {
   return (
-    <div className="container w-full mx-auto">
-      <footer className="py-6  text-center">
-        <span>Made with ❤ by️ Matthias Heisterkamp</span>
-      </footer>
-    </div>
+    <footer className="py-8">
+      <div className="container text-center rw-full mx-auto">
+        <div className="flex flex-wrap justify-center space-x-4">
+          <a
+            className="px-4 py-2 font-semibold"
+            href="https://twitter.com/mhstrkmp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            className="px-4 py-2 font-semibold"
+            href="https://github.com/mhstrkmp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            className="px-4 py-2 font-semibold"
+            href="https://www.linkedin.com/in/mhstrkmp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div className="container text-center text-sm py-4">
+          <span>&copy; {today.getFullYear()} Matthias Heisterkamp</span>
+        </div>
+      </div>
+    </footer>
   );
 };
 
